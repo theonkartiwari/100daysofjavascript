fbf296f4975a509a8a058b708b343452ed8dc43c
Onkar Tiwari Genius <theonkartiwari@gmail.com>
2024-01-13 15:55:15 +0530

    CHanges done

--- RandomNumGenerator/script.js
+++ RandomNumGenerator/script.js
@@ -1,11 +1,13 @@
 const number = document.querySelector(".number");
 const btn = document.querySelector(".generate");
+console.log("I am born genius");
 
 
 btn.addEventListener("click",()=>{
     const randomNumber = Math.floor(Math.random()*10+1);
     console.log(randomNumber);
     number.innerHTML=randomNumber;
-    console.log("Created by Onkar");
+    console.log("Created by Shweta");
+
  });
 
